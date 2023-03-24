cc9d6c2d32a8cd6a54cbe2222334e86f86021566
Daria <daria@example.com>
2023-03-24 22:15:28 +0100

    commit na pierwszym testowym branchu

--- src/Bmi.java
+++ src/Bmi.java
@@ -15,6 +15,5 @@ public class Bmi {
         System.out.println("twoje bmi: " + bmi);
         System.out.println("masz nadwagę: " + (bmi >= 25));
         System.out.println("masz niedowagę: " + (bmi <= 18.49));
-        
     }
 }
